8d98fadfa529e5b21763557dc3ac0341cb0b6ac9
Synder098 <166362004+Synder098@users.noreply.github.com>
2024-04-26 10:58:40 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
+const authRoute = require("./routes/auth");
+const cookieSession = require("cookie-session");
+const passportStrategy = require("./passport");
+const app = express();
+app.use(
+	cors({
+		origin: "http://localhost:3000",
+		credentials: true,
+	})
+);
+// app.use(
+// 	cors({
+// 	  credentials: true,
+// 	  // origin: "http://localhost:3000/",
+// 	  origin: process.env.Client_URL,
+// 	})
+//   );
+app.use(
+	cookieSession({
+		name: "session",
+		keys: ["cyberwolve"],
+		maxAge: 24 * 60 * 60 * 100,
+	})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+app.use("/auth", authRoute);
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listenting on port ${port}...`));
